c6a53f81682a05167c9a61b93bab26ecdf93de02
ashley <ashleybrookes86@gmail.com>
2023-04-02 23:08:47 +0100

    file upload now json Deserialized

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,9 +1,14 @@
-﻿namespace Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Models;
 
 public class Person
 {
+    [JsonIgnore]
     public int Id { get; set; }
+    [JsonPropertyName("firstName")]
     public string FirstName { get; set; }
+    [JsonPropertyName("lastName")]
     public string LastName { get; set; }
     public int GMC { get; set; }
 }
---
+++ Models/PersonImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+
+    public class PersonsImport
+    {
+        List<PersonWithAddress>? Persons{ get; set; }
+    }
+
+    public class PersonWithAddress :Person
+    {
+        public List<Address>? address { get; set; }
+    }
+}
