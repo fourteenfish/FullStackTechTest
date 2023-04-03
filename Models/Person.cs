fcb6bfccc64f97bb5e837b5fa2467442b21464ae
ashley <ashleybrookes86@gmail.com>
2023-04-03 21:02:15 +0100

    person validation

--- Models/Person.cs
+++ Models/Person.cs
@@ -11,4 +11,26 @@ public class Person
     [JsonPropertyName("lastName")]
     public string LastName { get; set; }
     public int GMC { get; set; }
+
+    public bool ValidatePerson(Person person)
+    {
+        if (person.FirstName.Length > 50)
+        {
+            throw new ArgumentOutOfRangeException("FirstName needs to less than 50 characters");
+        }
+        if (person.LastName.Length > 50)
+        {
+            throw new ArgumentOutOfRangeException("LastName needs to less than 50 characters");
+        }
+        if (person.GMC > 0)
+        {
+            if (person.GMC.ToString().Length > 7)
+            {
+                throw new ArgumentOutOfRangeException("GMC must be 7 digits");
+            }
+        }
+
+        return true;
+
+    }
 }
