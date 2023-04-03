5b9b6c4dd6d9f9080327977d7b4735ba0d30b3fa
ashley <ashleybrookes86@gmail.com>
2023-04-03 22:07:27 +0100

    address validation with built in attributes

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Models;
@@ -6,13 +7,24 @@ public class Address
 {
     [JsonIgnore]
     public int Id { get; set; }
+    
     [JsonIgnore]
     public int PersonId { get; set; }
+    
     [JsonPropertyName("line1")]
+    [Required]
+    [MaxLength(200)]
     public string Line1 { get; set; }
+    
     [JsonPropertyName("city")]
+    [Required]
+    [MaxLength(100)]
     public string City { get; set; }
+    
     [JsonPropertyName("postcode")]
+    [Required]
+    [MinLength(8)]
+    [MaxLength(8)]
     public string Postcode { get; set; }
     
     // assume all records will be UK, i.e. disregard "Country" field
